94e4881feff2f164c361c201e96b09110eb0bdbb
Rennan Santos <rennansantos@outlook.com>
2023-12-25 16:45:51 -0300

    1.0

---
+++ lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:state_manage/setState/imc_setstate_page.dart';
+import 'package:state_manage/value_notifier/value_notifier_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () => _goToPage(context, ImcSetstatePage()),
+                child: Text('SetState')),
+            ElevatedButton(
+                onPressed: () => _goToPage(
+                      context,
+                      ValueNotifierPage(),
+                    ),
+                child: Text('ValueNotifier')),
+            ElevatedButton(onPressed: () {}, child: Text('ChangeNotifier')),
+            ElevatedButton(
+                onPressed: () {}, child: Text('Bloc Pattern (Streams)'))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/value_notifier/value_notifier_page.dart
@@ -0,0 +1,123 @@
+import 'dart:math';
+
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:state_manage/widgets/imc_gauge_range.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+import '../widgets/imc_gauge.dart';
+
+class ValueNotifierPage extends StatefulWidget {
+  const ValueNotifierPage({super.key});
+
+  @override
+  State<ValueNotifierPage> createState() => _ValueNotifierPageState();
+}
+
+class _ValueNotifierPageState extends State<ValueNotifierPage> {
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  var imc = ValueNotifier(0.0);
+
+  Future<void> _calcularIMC(
+      {required double peso, required double altura}) async {
+    imc.value = 0.0;
+
+    await Future.delayed(Duration(seconds: 1));
+
+    imc.value = peso / pow(altura, 2);
+  }
+
+  @override
+  void dispose() {
+    pesoEC.dispose();
+    alturaEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Imc SetState'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ValueListenableBuilder<double>(
+                    valueListenable: imc,
+                    builder: (_, imcvalue, __) {
+                      return ImcGauge(
+                        imc: imcvalue,
+                      );
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: pesoEC,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: 'Peso'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                        locale: 'pt_BR',
+                        symbol: '',
+                        decimalDigits: 2,
+                        turnOffGrouping: true)
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Peso Obrigatorio';
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: alturaEC,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: 'Altura'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    )
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Altura Obrigatoria';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var formValid = formKey.currentState?.validate() ?? false;
+                      if (formValid) {
+                        var formatter = NumberFormat.simpleCurrency(
+                            locale: 'pt_BR', decimalDigits: 2);
+                        double peso = formatter.parse(pesoEC.text) as double;
+                        double altura =
+                            formatter.parse(alturaEC.text) as double;
+
+                        _calcularIMC(peso: peso, altura: altura);
+                      }
+                    },
+                    child: Text('Calcular IMC'))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
