e07119682669f3b2c54f4070d26541477320ab0a
Rennan Santos <rennansantos@outlook.com>
2023-12-28 20:07:10 -0300

    correct2.0

--- lib/bloc_pattern/controller.dart
+++ lib/bloc_pattern/controller.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:state_manage/bloc_pattern/state.dart';
 
@@ -8,15 +9,8 @@ class ImcBlocPatternController {
     ..add(ImcState(imc: 0));
   Future<void> calcularImc(
       {required double peso, required double altura}) async {
-    try {
-      _imcStreamController.add(ImcStateLoading());
-      await Future.delayed(const Duration(seconds: 1));
-      final imc = peso / pow(altura, 2);
-      // throw Exception();
-      _imcStreamController.add(ImcState(imc: imc));
-    } on Exception catch (e) {
-      _imcStreamController.add(ImcStateError(messege: 'Erro ao calcular IMC'));
-    }
+   
+     
   }
 
   Stream<ImcState> get imcOut => _imcStreamController.stream;
